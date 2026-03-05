f2275d986e308f75aa56f4bb59d0078821715338
DarshiDoshi <102381142+DarshiDoshi@users.noreply.github.com>
2026-03-05 19:21:02 +0530

    Update and rename dag1.py to test_dag_1.py

--- dags/test_dag_1.py
+++ dags/test_dag_1.py
@@ -9,7 +9,7 @@ default_args = {
 }
 
 @dag(
-    dag_id="dag1",
+    dag_id="test_dag_1",
     default_args=default_args,
     description="A simple example DAG for Airflow 3",
     schedule="@daily",
